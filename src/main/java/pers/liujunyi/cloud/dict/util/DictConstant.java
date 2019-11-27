6750cb0e716ccfb593ba7618e586f3962ba7f5f5
lbyjwwyqt147 <ljy671018>
2019-11-27 16:03:59 +0800

    初始化

---
+++ src/main/java/pers/liujunyi/cloud/dict/util/DictConstant.java
@@ -0,0 +1,12 @@
+package pers.liujunyi.cloud.dict.util;
+
+/**
+ * 常量信息
+ * @author ljy
+ */
+public class DictConstant {
+    /** 禁用状态 */
+    public static final Byte DISABLE_STATUS = 1;
+    /** 启用状态 */
+    public static final Byte ENABLE_STATUS = 0;
+}
---
+++ src/test/java/pers/liujunyi/cloud/dict/Test.java
@@ -0,0 +1,9 @@
+package pers.liujunyi.cloud.dict;
+
+
+public class Test {
+
+    public static void main(String[] ars) {
+       
+    }
+}
